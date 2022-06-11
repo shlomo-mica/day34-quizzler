4884a52b8f3b88e203388c1b95d28991bc945115
shlomo-mica <shlomo.mica@outlook.co.il>
2022-06-11 14:40:47 +0300

    Add files via upload

--- ui.py
+++ ui.py
@@ -14,15 +14,16 @@ class Quizeller():
         self.canvas = Canvas()
         self.window.title("Quizzer")
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
-        self.canvas.config(width=300, height=270, background="WHITE", )
-        self.h = quiz
-        question_text = quiz.next_question()
-        self.user_answer = quiz.check_answer('True')
-
+        self.quiz_object = quiz
+        # self.question_text = quiz.next_question()
+        # self.user_answer = quiz.check_answer('True')
         # self.canvas.create_text(122,122, text='A wonderful', font=('Ariel',20,'italic'), fill='red',anchor="center")
-        canvas2 = self.canvas.create_text(152, 122, font=('Ariel', 18, 'italic'), fill='red', anchor="center",
-                                          width=250)
-        self.canvas.itemconfig(canvas2, text=question_text)
+        self.canvas.config(width=300, height=270, background="WHITE", )
+
+        self.new_question = self.canvas.create_text(152, 122, font=('Ariel', 18, 'italic'), fill='black',
+                                                    anchor="center",
+                                                    width=250, text="Welcome to trivia game")
+        # self.canvas.itemconfig(canvas2, text=question_text)
 
         self.score_label = Label(background=THEME_COLOR, fg="white", text=f"SCORE ={quiz.update_score()}",
                                  font=('Ariel', 15, 'italic'))
@@ -31,26 +32,50 @@ class Quizeller():
         #
         self.true_button = Button(image=true_pict, command=(self.True_button))
 
-        self.false_button = Button(image=false_pict, )
+        self.false_button = Button(image=false_pict, command=self.False_button)
 
         self.score_label.grid(row=0, column=0, columnspan=2, pady=30)
         self.canvas.grid(row=1, column=1)
         self.true_button.grid(row=3, column=0, sticky='w', columnspan=2, pady=22)
         self.false_button.grid(row=3, column=1, columnspan=1, sticky='e', pady=22)
 
+        self.get_new_question()
+
         self.window.mainloop()
-    
+
+    def get_new_question(self):
+
+            a = self.quiz_object.next_question()
+            self.canvas.itemconfig(self.new_question, text=a)
+
     def True_button(self):
-        self.h.check_answer('True')
-        self.score_label.config(text=self.h.update_score())
-        self.h.next_question()
-         
+        if self.quiz_object.check_answer('True'):
+            self.update_score_color('green')
 
 
 
+        else:
+            self.canvas.config(background="red")
+            self.canvas.after(1000, self.back_to_white)
 
-# def change_quastion(self):
+    def False_button(self):
+        if self.quiz_object.check_answer('False'):
+            self.update_score_color('green')
+        else:
+            self.canvas.config(background="red")
+            self.canvas.after(1000, self.back_to_white)
 
+    def back_to_white(self):
+        if self.quiz_object.still_has_questions():
+            self.canvas.config(bg="white")
+            self.canvas.itemconfig(self.new_question, text=self.quiz_object.next_question())
+        else:
+            self.window.destroy()    #finish the trivia game
+
+    def update_score_color(self, color):
+        self.score_label.config(text=f"score--->{self.quiz_object.update_score()}")
+        self.canvas.config(background=color)
+        self.canvas.after(2000, self.back_to_white)
 
 # BACKGROUND_COLOR = "#B1DDC6"Q
 
